d1c246f3fe23fe883c7f91b1762a2f9cd8db0607
qaaziabdullah-eng <qaaziabdullah@gmail.com>
2026-01-06 18:17:56 +0500

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,5 @@
+def wishes(n):
+    print("hello",n,"how are you")
+    print("hope you are doing well")
+
+    wishes("ahad")
---
+++ hw.py
@@ -0,0 +1,8 @@
+#ACP-circumference
+
+import math
+def cir():
+    r=float(input("enter radius"))
+    print("circumference")
+
+    cir()
